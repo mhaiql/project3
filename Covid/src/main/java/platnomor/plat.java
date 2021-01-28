19ed8d730723d14cd2b0c4f72a96b44ac1908755
Muhammad Haiqal <67793226+mhaiql@users.noreply.github.com>
2021-01-28 15:21:02 +0700

    Mengganti tulisan

--- Covid/src/main/java/platnomor/plat.java
+++ Covid/src/main/java/platnomor/plat.java
@@ -21,7 +21,7 @@ public class plat {
     public static void main(String[] args) {
     int a;
     Scanner scan = new Scanner(System.in);
-    System.out.print("Masukkan nomor plat kendaraan : ");
+    System.out.print("Masukkan nomor");
     a = scan.nextInt();
     if (a % 2 == 0) {
       System.out.print("Bilangan " + a + " adalah genap.\n");
